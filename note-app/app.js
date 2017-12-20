93425d2e0be1075e3d8cfd955911835bb6b5c7af
Yue Gu <yuegu1993@gmail.com>
2017-12-20 10:50:36 -0800

    using yargs for inputs

--- note-app/app.js
+++ note-app/app.js
@@ -2,20 +2,23 @@ console.log('Starting app.')
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2]
+const argv = yargs.argv;
+var command = argv._[0]
 console.log('Command: ', command);
+console.log('yargs', argv);
 
 if (command === 'add') {
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 }else if(command === 'list') {
-  console.log('Listing all notes')
+  notes.getAll();
 }else if(command === 'read'){
-  console.log('fetching note')
+  notes.getNote(argv.title);
 }else if(command === 'remove'){
-  console.log('removing note')
+  notes.removeNote(argv.title);
 }else{
   console.log('Command not recognized')
 }
--- note-app/notes.js
+++ note-app/notes.js
@@ -1,1 +1,24 @@
 console.log('Starting notes.js'); 
+
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+  console.log('Getting Note', title)
+}
+
+var removeNote = (title) => {
+  console.log('Removing Note', title)
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
