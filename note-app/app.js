119050e5dcbb4f89eae924104c734bfe0802772d
Yue Gu <yuegu1993@gmail.com>
2017-12-21 16:19:19 -0800

    requiring arguments and advanced yargs

--- note-app/app.js
+++ note-app/app.js
@@ -3,14 +3,38 @@ const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
+const title = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
+const body = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+}
 
-const argv = yargs.argv;
+const argv = yargs
+  .command('add', 'add a new note', {
+    title,
+    body
+  })
+  .command('list', 'list all notes')
+  .command('read', 'read a note', {
+    title
+  })
+  .command('remove', 'remove a note', {
+    title
+  })
+  .help()
+  .argv;
 var command = argv._[0]
 
 if (command === 'add') {
-  if(notes.addNote(argv.title, argv.body)){
+  let note = notes.addNote(argv.title, argv.body)
+  if(note){
     console.log('add successful');
-    notes.logNote(notes.addNote(argv.title, argv.body));
+    notes.logNote(note);
   }else{
     console.log('duplicate name');
   };
@@ -19,9 +43,10 @@ if (command === 'add') {
   console.log(`Printing ${allNotes.length} note(s)`)
   allNotes.forEach(note => console.log(notes.logNote(note)));
 }else if(command === 'read'){
-  if(notes.getNote(argv.title)){
+  let note = notes.getNote(argv.title)
+  if(note){
     console.log('note read')
-    notes.logNote(notes.getNote(argv.title));
+    notes.logNote(note);
   }else{
     console.log('note not found')
   }
