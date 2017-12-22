b772de0b9f24ebbf540a36b839bc9a2b24e3239d
Yue Gu <yuegu1993@gmail.com>
2017-12-22 12:40:17 -0800

    encoding user input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,23 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs 
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+  
+console.log(argv)
 
 request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyB5PTY5AWe7WckkJBeX9_O8enntD577o8A',
+  url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}&key=AIzaSyB5PTY5AWe7WckkJBeX9_O8enntD577o8A`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`)
