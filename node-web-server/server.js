d16eb8cc58d633fb46f7c61dd82c0f37458270a7
Yue Gu <yuegu1993@gmail.com>
2018-06-15 11:14:46 -0700

    5-45 : express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log.')
+        }
+    })
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
     welcomeMsg: 'Youkoso'
   });
 })
@@ -17,7 +43,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 })
 
