e0205465894265e74a4bf301b91b7573ea5f784c
Yue Gu <yuegu1993@gmail.com>
2018-06-20 12:55:20 -0700

    post todo api route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,54 +1,27 @@
-var mongoose = require('mongoose');
+var express = require('express')
+var bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } =  require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    })
+    
+    todo.save().then(doc => {
+        res.send(doc)
+    }, (e) => {
+        res.status(400).send(e);
+    })
 })
 
-// var newTodo = new Todo({
-//     text: 'Cook dinner'
-// });
-// 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// })
 
-// var newTodo = new Todo({
-//     text: 'Do laundry',
-//     completed: true,
-//     completedAt: 1529516275
-// });
-// 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// })
-
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+app.listen(3000, () => {
+    console.log('started in port 3000');
 })
-
-var newUser = new User({
-    email: 'hunter2@gmail.com'
-})
-
-newUser.save().then(doc => console.log(doc))
