ba18bb18235aeb5678b7d8f9099a7376ab50cb4e
Yue Gu <yuegu1993@gmail.com>
2018-06-25 14:23:59 -0700

    get todo/id route and tests

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var { ObjectID } = require('mongodb')
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -28,6 +29,23 @@ app.get('/todos', (req, res) => {
         })
     }, e => {
         res.status(400).send(e);
+    })
+})
+
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    req.params.id
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({})
+    }
+    
+    let todo = Todo.findById(id).then(todo => {
+        if(!todo){
+            return res.status(404).send({});
+        }
+        res.send({ todo })
+    }, e => {
+        return res.status(400).send(e);
     })
 })
 
